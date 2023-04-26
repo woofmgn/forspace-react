26c7dd4242af76b678d46d83f0628ef9126b5b46
Denis Vakulenko <101655241+woofmgn@users.noreply.github.com>
2023-04-26 22:15:50 +0300

    fix: stylelint disabled

--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from 'react-i18next';
-import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ThemeButton } from '../Button/Button';
-import cls from './LangSwitcher.module.scss';
+// import cls from './LangSwitcher.module.scss';
 
 interface LangSwitcherProps {
   className?: string;
@@ -16,7 +15,7 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
 
   return (
     <Button
-      className={classNames(cls.LangSwitcher, {}, [className])}
+      // className={classNames(cls.LangSwitcher, {}, [className])}
       theme={ThemeButton.CLEAR}
       onClick={toggle}
     >
